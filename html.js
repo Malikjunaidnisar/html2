c59f2b1f0e7638b1a4358b207dae8bc61b35117e
Malikjunaidnisar <sayaandtrain1@gmail.com>
2025-02-10 22:03:46 +0500

    js

---
+++ html.js
@@ -0,0 +1,10 @@
+a="1x1=1\n1x2=2\n1x3=3\n1x4=4\n1x5=5\n1x6=6\n1x7=7\n1x8=8\n1x9=9\n1x10=10"
+b="2x1=2\n2x2=4\n2x3=6\n2x4=8\n2x5=10\n2x6=12\n2x7=14\n2x8=16\n2x9=18\n2x10=20"
+c="3x1=3\n3x2=6\n3x3=9\n3x4=12\n3x5=15\n3x6=18\n3x7=21\n3x8=24\n3x9=27\n3x10=30"
+d="4x1=4\n4x2=8\n4x3=12\n4x4=16\n4x5=20\n4x6=24\n4x7=28\n4x8=32\n4x9=36\n4x10=40"
+e="5x1=5\n5x2=10\n5x3=15\n5x4=20\n5x5=25\n5x6=30\n5x7=35\n5x8=40\n5x9=45\n5x10=50"
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
